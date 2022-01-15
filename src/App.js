2f43d74eaade9fa9f5d6afd0a12c37216fb0ef15
Bryce Klinker <bryce.klinker@gmail.com>
2022-01-15 15:50:17 -0600

    Almost finished evaluating the answer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './math_app_logo.jpg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+    const [awnser, setAwnser] = useState(0)
+    const[inCorrect, setIsCorrect] = useState(null)
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    20x5=<input type={'number'} />
-                </p>
+                <form onSubmit={evt=>{
+                    if (awnser == 100){
+                        setIsCorrect(true)
+                    }else{setIsCorrect(false)}
+                }}>
+                    20x5=<input onChange={evt => setAwnser(evt.target.value)} value={awnser} className={'answer-input'} type={'number'} />
+                </form>
             </header>
         </div>
     );
